ee7c050d15006bde966c14eac6ce40410086e000
Matt Coffin <mcoffin13@gmail.com>
2024-05-30 08:38:33 -0600

    20240530

--- scripts/base16-woodland.sh
+++ scripts/base16-woodland.sh
@@ -5,11 +5,13 @@
 
 color00="23/1e/18" # Base 00 - Black
 color01="d3/5c/5c" # Base 08 - Red
-color02="b7/ba/53" # Base 0B - Green
+#color02="b7/ba/53" # Base 0B - Green
+color02="6e/b9/58" # Base 0B - Green
 color03="e0/ac/16" # Base 0A - Yellow
 color04="88/a4/d3" # Base 0D - Blue
 color05="bb/90/e2" # Base 0E - Magenta
-color06="6e/b9/58" # Base 0C - Cyan
+#color06="6e/b9/58" # Base 0C - Cyan
+color06="1f/ad/83" # Base 0C - Cyan
 color07="ca/bc/b1" # Base 05 - White
 color08="9d/8b/70" # Base 03 - Bright Black
 color09=$color01 # Base 08 - Bright Red
